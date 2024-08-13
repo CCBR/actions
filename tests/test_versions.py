bb3d44abba781f8da7eb3ce39000210809b643cd
Kelly Sovacool <kelly.sovacool@nih.gov>
2024-08-13 13:12:09 -0400

    test: minimal tests for docs

--- tests/test_versions.py
+++ tests/test_versions.py
@@ -1,9 +1,9 @@
-from ccbr_actions.versions import get_releases, get_latest_release_tag
+from ccbr_actions.versions import get_releases, get_latest_release_tag, match_semver
 
 
 def test_get_releases():
-    print(get_releases(limit=1))
+    assert match_semver(get_releases(limit=1)[0]["tagName"], with_leading_v=True)
 
 
 def test_get_latest_release_tag():
-    print(get_latest_release_tag())
+    assert match_semver(get_latest_release_tag(), with_leading_v=True)
