7ed2534d4f573314e07754f5edaecd923e797868
Kelly Sovacool <kelly.sovacool@nih.gov>
2024-08-12 21:55:53 -0400

    feat: create post-release action

--- src/ccbr_actions/release.py
+++ src/ccbr_actions/release.py
@@ -11,64 +11,6 @@ from .util import shell_run, precommit_run
 from .versions import check_version_increments_by_one, match_semver, get_current_hash
 
 
-def prepare_draft_release(
-    next_version_manual="${{ github.event.inputs.version_tag }}",
-    next_version_convco="${{ steps.semver.outputs.next }}",
-    current_version="${{ steps.semver.outputs.current }}",
-    gh_event_name="${{ github.event_name }}",
-    changelog_filepath="CHANGELOG.md",
-    dev_header="development version",
-    release_notes_filepath=".github/latest-release.md",
-    version_filepath="VERSION",
-    citation_filepath="CITATION.cff",
-    release_branch="release-draft",
-    pr_ref_name="${{ github.ref_name }}",
-    repo="${{ github.event.repository.name }}",
-    debug=False,
-):
-    next_version = get_release_version(
-        next_version_manual=next_version_manual,
-        next_version_convco=next_version_convco,
-        current_version=current_version,
-        gh_event_name=gh_event_name,
-    )
-    next_version_strict = next_version.lstrip("v")
-    set_output("NEXT_VERSION", next_version)
-
-    changelog_lines, next_release_lines = get_changelog_lines(
-        latest_version=get_latest_release_tag().lstrip("v"),
-        next_version=next_version_strict,
-        changelog_filepath=changelog_filepath,
-        dev_header=dev_header,
-    )
-
-    with open(release_notes_filepath, "w") as outfile:
-        outfile.writelines(next_release_lines)
-    with open(changelog_filepath, "w") as outfile:
-        outfile.writelines(changelog_lines)
-    with open(version_filepath, "w") as outfile:
-        outfile.write(f"{next_version_strict}\n")
-
-    update_citation(citation_file=citation_filepath, next_version=next_version)
-    changed_files = [citation_filepath, changelog_filepath, version_filepath]
-    precommit_run(f'--files {" ".join(changed_files)}')
-    push_release_draft_branch(
-        release_branch=release_branch,
-        pr_ref_name=pr_ref_name,
-        next_version=next_version,
-        files=changed_files,
-        debug=debug,
-    )
-    create_release_draft(
-        release_branch=release_branch,
-        next_version=next_version,
-        release_notes_filepath=release_notes_filepath,
-        release_target=get_current_hash(),
-        repo=repo,
-        debug=debug,
-    )
-
-
 def get_releases(limit=1, args="", json_fields="name,tagName,isLatest,publishedAt"):
     """
     Get a list of releases from GitHub.
@@ -284,3 +226,97 @@ def create_release_draft(
         print(cmd)
     else:
         shell_run(cmd)
+
+
+def prepare_draft_release(
+    next_version_manual="${{ github.event.inputs.version_tag }}",
+    next_version_convco="${{ steps.semver.outputs.next }}",
+    current_version="${{ steps.semver.outputs.current }}",
+    gh_event_name="${{ github.event_name }}",
+    changelog_filepath="CHANGELOG.md",
+    dev_header="development version",
+    release_notes_filepath=".github/latest-release.md",
+    version_filepath="VERSION",
+    citation_filepath="CITATION.cff",
+    release_branch="release-draft",
+    pr_ref_name="${{ github.ref_name }}",
+    repo="${{ github.event.repository.name }}",
+    debug=False,
+):
+    next_version = get_release_version(
+        next_version_manual=next_version_manual,
+        next_version_convco=next_version_convco,
+        current_version=current_version,
+        gh_event_name=gh_event_name,
+    )
+    next_version_strict = next_version.lstrip("v")
+    set_output("NEXT_VERSION", next_version)
+
+    changelog_lines, next_release_lines = get_changelog_lines(
+        latest_version=get_latest_release_tag().lstrip("v"),
+        next_version=next_version_strict,
+        changelog_filepath=changelog_filepath,
+        dev_header=dev_header,
+    )
+
+    with open(release_notes_filepath, "w") as outfile:
+        outfile.writelines(next_release_lines)
+    with open(changelog_filepath, "w") as outfile:
+        outfile.writelines(changelog_lines)
+    with open(version_filepath, "w") as outfile:
+        outfile.write(f"{next_version_strict}\n")
+
+    update_citation(citation_file=citation_filepath, next_version=next_version)
+    changed_files = [citation_filepath, changelog_filepath, version_filepath]
+    precommit_run(f'--files {" ".join(changed_files)}')
+    push_release_draft_branch(
+        release_branch=release_branch,
+        pr_ref_name=pr_ref_name,
+        next_version=next_version,
+        files=changed_files,
+        debug=debug,
+    )
+    create_release_draft(
+        release_branch=release_branch,
+        next_version=next_version,
+        release_notes_filepath=release_notes_filepath,
+        release_target=get_current_hash(),
+        repo=repo,
+        debug=debug,
+    )
+
+
+def post_release_cleanup(
+    changelog_filepath="CHANGELOG.md",
+    repo="${{ github.event.repository.name }}",
+    release_tag="${{ github.ref_name }}",
+    pr_branch="${{ inputs.branch }}",
+    pr_reviewer="${{ github.triggering_actor }}",
+    draft_branch="release-draft",
+    dev_header="development version",
+    version_filepath="VERSION",
+    citation_filepath="CITATION.cff",
+):
+    with open(changelog_filepath, "r") as infile:
+        lines = infile.readlines()
+    lines.insert(0, f"## { repo } {dev_header}\n\n")
+    with open(changelog_filepath, "w") as outfile:
+        outfile.writelines(lines)
+
+    with open(version_filepath, "r") as infile:
+        version = infile.read().strip()
+    with open(version_filepath, "w") as outfile:
+        outfile.write(f"{version}-dev\n")
+
+    changed_files = " ".join([citation_filepath, changelog_filepath, version_filepath])
+    precommit_run(f"--files {changed_files}")
+    shell_run(
+        f"""git add {changed_files}
+git commit -m "chore: bump changelog & version after release of { release_tag }"
+git push --set-upstream origin { pr_branch }
+gh pr create \
+    --fill-first \
+    --reviewer { pr_reviewer }
+git push origin --delete {draft_branch} || echo "No {draft_branch} branch to delete"
+"""
+    )
