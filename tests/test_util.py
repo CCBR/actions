0b4d2c3d94d920798ee4191dd459e27301efe203
Kelly Sovacool <kelly.sovacool@nih.gov>
2024-09-13 15:38:21 -0400

    test: path_resolve() allows writing to symlink targets

--- tests/test_util.py
+++ tests/test_util.py
@@ -1,3 +1,8 @@
+import os
+import pathlib
+import shutil
+import tempfile
+
 from ccbr_actions.util import date_today, precommit_run, path_resolve
 
 
@@ -7,3 +12,16 @@ def test_precommit():
 
 def test_path_resolve_symlink():
     assert str(path_resolve("tests/data/file_ln")).endswith("tests/data/file.txt")
+
+
+def test_path_resolve_write():
+    with tempfile.TemporaryDirectory() as tmp_dir:
+        shutil.copy("tests/data/file.txt", tmp_dir)
+        src_path = pathlib.Path(tmp_dir) / "file.txt"
+        link_path = pathlib.Path(tmp_dir) / "file_link"
+        os.symlink(src_path, link_path)
+        with open(path_resolve(link_path), "w") as outfile:
+            outfile.write("hello world")
+        with open(src_path, "r") as infile:
+            text = infile.read()
+    assert text == "hello world"
