7f3d850ac6785b839d261c6bea9144c9e84ea488
Kelly Sovacool <kelly.sovacool@nih.gov>
2024-09-13 16:01:22 -0400

    test: use test/data files for tests
    to avoid accidentally overwriting real repo files

--- tests/test_release.py
+++ tests/test_release.py
@@ -21,11 +21,11 @@ def test_prepare_draft_release():
         next_version_convco="v1.0.0",
         current_version="v0.9.10",
         gh_event_name="push",
-        changelog_filepath="CHANGELOG.md",
+        changelog_filepath="tests/data/example_changelog.md",
         dev_header="development version",
-        release_notes_filepath=".github/latest-release.md",
-        version_filepath="VERSION",
-        citation_filepath="CITATION.cff",
+        release_notes_filepath="tests/data/.github/latest-release.md",
+        version_filepath="tests/data/VERSION",
+        citation_filepath="tests/data/CITATION.cff",
         release_branch="release-draft",
         pr_ref_name="PR_BRANCH_NAME",
         repo="CCBR/actions",
@@ -45,12 +45,12 @@ def test_create_release_draft():
         create_release_draft,
         release_branch="release-draft",
         next_version="v1",
-        release_notes_filepath="CHANGELOG.md",
+        release_notes_filepath="tests/data/example_changelog.md",
         release_target="HEAD",
         repo="CCBR/actions",
         debug=True,
     ).startswith(
-        "gh release create v1 --draft --notes-file CHANGELOG.md --title 'actions 1' --repo CCBR/actions --target HEAD\n"
+        "gh release create v1 --draft --notes-file tests/data/example_changelog.md --title 'actions 1' --repo CCBR/actions --target HEAD\n"
     )
 
 
@@ -60,14 +60,14 @@ def test_push_release_draft_branch():
         release_branch="release-draft",
         pr_ref_name="v1",
         next_version="v1",
-        files=["CHANGELOG.md"],
+        files=["tests/data/example_changelog.md"],
         debug=True,
     ).startswith(
         """git push origin --delete release-draft || echo "No release-draft branch to delete"
 git switch -c release-draft || git switch release-draft
 git merge --ff-only v1
 
-git add CHANGELOG.md
+git add tests/data/example_changelog.md
 git commit -m 'chore: 🤖 prepare release v1'
 git push --set-upstream origin release-draft
 
