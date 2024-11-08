47af2153c8d82137052399f8cd77ecde3d6643b3
Kelly Sovacool <kelly.sovacool@nih.gov>
2024-11-08 12:06:44 -0500

    fix: add --output option for print_versions.py

--- scripts/print_versions.py
+++ scripts/print_versions.py
@@ -14,42 +14,67 @@ parser.add_argument(
     type=str,
     help="Path to the JSON file containing commands and their version commands.",
 )
-args = parser.parse_args()
+parser.add_argument(
+    "--output",
+    required=False,
+    type=str,
+    help="Output file path. If not specified, output will be printed to stdout.",
+)
 
-# Load JSON data
-with open(args.json, "r") as file:
-    commands = json.load(file)
 
-# Prepare table header
-print("")
-print("| Tool | Version |")
-print("|---------|---------|")
+def create_table(args):
+    # Load JSON data
+    with open(args.json, "r") as file:
+        commands = json.load(file)
 
-# Run each command and capture output
-for command_name, version_command in commands.items():
-    if shutil.which(command_name):
-        try:
-            # Run the command and capture its output
-            result = subprocess.run(
-                version_command, shell=True, check=True, text=True, capture_output=True
-            )
-            version_info = (
-                result.stdout.strip() if result.stdout else result.stderr.strip()
-            )
-            version_info = (
-                version_info.strip()
-                .replace('"', "")
-                .replace("'", "")
-                .replace("(", "")
-                .replace(")", "")
-            )
-            version_info = version_info.strip() or "VERSIONUNKNOWN"
-        except subprocess.CalledProcessError:
-            # If the command doesn't exist, handle the exception
+    # Prepare table header
+    output_list = ["", "| Tool | Version |", "|---------|---------|"]
+
+    # Run each command and capture output
+    for command_name, version_command in commands.items():
+        if shutil.which(command_name):
+            try:
+                # Run the command and capture its output
+                result = subprocess.run(
+                    version_command,
+                    shell=True,
+                    check=True,
+                    text=True,
+                    capture_output=True,
+                )
+                version_info = (
+                    result.stdout.strip() if result.stdout else result.stderr.strip()
+                )
+                version_info = (
+                    version_info.strip()
+                    .replace('"', "")
+                    .replace("'", "")
+                    .replace("(", "")
+                    .replace(")", "")
+                )
+                version_info = version_info.strip() or "VERSIONUNKNOWN"
+            except subprocess.CalledProcessError:
+                # If the command doesn't exist, handle the exception
+                version_info = "NOTINDOCKER"
+        else:
             version_info = "NOTINDOCKER"
-    else:
-        version_info = "NOTINDOCKER"
-    # Print in Markdown table format
-    print(f"| {command_name} | {version_info} |")
+        # Print in Markdown table format
+        output_list.append(f"| {command_name} | {version_info} |")
+    output_list.append("")
+    return output_list
 
-print("")
+
+def main(args):
+    output_list = create_table(args)
+    output_str = "\n".join(output_list)
+
+    if args.output:
+        with open(args.output, "w") as outfile:
+            outfile.write(output_str)
+    else:
+        print(output_str, end="")
+
+
+if __name__ == "__main__":
+    args = parser.parse_args()
+    main(args)
